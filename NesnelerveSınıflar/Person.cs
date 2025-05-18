5f30d1fd4ab9c8f34d793ded8a735bd996c4bf30
dnzoncuu <berkoncu17@gmail.com>
2025-05-18 13:49:01 +0300

    Add project files.

---
+++ NesnelerveSınıflar/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesnelerveSınıflar
+{
+    public class Person//Class oluşturuldu.
+    {
+        //Fields Oluşturuldu.
+        private string _name; 
+        private string _lastName;
+        private DateTime _birthDate;
+
+        //properties tanımlandı.
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string lastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime birthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+        //Metot tanımlandı.
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Adınız : {name}");
+            Console.WriteLine($"Soyadınız : {lastName}");
+            Console.WriteLine($"Doğum Tarihi : {birthDate.ToShortDateString()}");
+        }
+    }
+}
---
+++ NesnelerveSınıflar/Program.cs
@@ -0,0 +1,17 @@
+﻿using NesnelerveSınıflar;
+
+Person ogrenci = new Person();//Nesne Oluşturuldu.
+
+ogrenci.name = "Deniz";
+ogrenci.lastName = "Öncü";
+ogrenci.birthDate = new DateTime(2002, 03, 16);
+
+ogrenci.BilgileriYazdir();//Metot Çağrıldı.
+
+Person ogretmen =new Person();
+
+ogretmen.name = "Sergen";
+ogretmen.lastName = "Yanlçın";
+ogretmen.birthDate = new DateTime(1972,11,05);
+
+ogretmen.BilgileriYazdir();//Metot Çağrıldı.
